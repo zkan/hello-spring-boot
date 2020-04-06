989234fc57d3f36bde3c984a451d70b942a4b54e
zkan <kan@prontomarketing.com>
2020-04-06 17:21:03 +0700

    :sparkles: Create speaker model :bear:

--- conference-demo/src/main/java/team/odds/conferencedemo/models/Session.java
+++ conference-demo/src/main/java/team/odds/conferencedemo/models/Session.java
@@ -12,6 +12,7 @@ public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
+
     private String session_name;
     private String session_description;
     private Integer session_length;
---
+++ conference-demo/src/main/java/team/odds/conferencedemo/models/Speaker.java
@@ -0,0 +1,61 @@
+package team.odds.conferencedemo.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long speaker_id;
+
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+
+    public Speaker() {
+
+    }
+
+    public Long getSpeaker_id() {
+        return speaker_id;
+    }
+
+    public void setSpeaker_id(Long speaker_id) {
+        this.speaker_id = speaker_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}

2efd9c2b33268a31a23ae4a425797cc4cdd8deff
zkan <kan@prontomarketing.com>
2020-04-06 18:13:18 +0700

    :sparkles: Create controllers for sessions and speakers :bear:

---
+++ conference-demo/src/main/java/team/odds/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,35 @@
+package team.odds.conferencedemo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import team.odds.conferencedemo.models.Session;
+import team.odds.conferencedemo.repositories.SessionRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    // by default, spring boot returns 200
+    @GetMapping
+    // we need to override the response code, so it returns 201
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+}
---
+++ conference-demo/src/main/java/team/odds/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,29 @@
+package team.odds.conferencedemo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.odds.conferencedemo.models.Speaker;
+import team.odds.conferencedemo.repositories.SpeakerRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+}

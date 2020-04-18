d5fcca0b41c043f8d395a72ca6667a07e87c42ad
zkan <zkan.cs@gmail.com>
2020-04-18 18:34:11 +0700

    :tada: Create project to try Mockito :bear:

---
+++ mockito/src/test/java/team/odds/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.odds.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

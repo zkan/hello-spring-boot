5fed0656a60e3f54daba35c80a5d09116cff4f1a
zkan <zkan.cs@gmail.com>
2021-03-06 21:17:48 +0700

    Return list of posts :bear:

--- blogs/src/main/kotlin/team/odds/blogs/controllers/BlogController.kt
+++ blogs/src/main/kotlin/team/odds/blogs/controllers/BlogController.kt
@@ -7,8 +7,9 @@ import team.odds.blogs.models.Post
 @RestController
 class BlogController {
     @GetMapping("/blogs")
-    fun getBlogs(): Post {
-        val post = Post(1, 1, "Hello", "This is my body")
-        return post
+    fun getBlogs(): List<Post> {
+        val post1 = Post(1, 1, "Hello", "This is my body")
+        val post2 = Post(2, 1, "Another Hello", "This is my another body")
+        return listOf(post1, post2)
     }
 }

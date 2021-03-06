e6616eeb1b843cab6f4e1820d533a0df94baf6eb
zkan <zkan.cs@gmail.com>
2021-03-06 21:16:02 +0700

    Create post model and return post in response :bear:

--- blogs/src/main/kotlin/team/odds/blogs/controllers/BlogController.kt
+++ blogs/src/main/kotlin/team/odds/blogs/controllers/BlogController.kt
@@ -2,11 +2,13 @@ package team.odds.blogs.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import team.odds.blogs.models.Post
 
 @RestController
 class BlogController {
     @GetMapping("/blogs")
-    fun getBlogs(): String {
-        return "Hey, World!"
+    fun getBlogs(): Post {
+        val post = Post(1, 1, "Hello", "This is my body")
+        return post
     }
 }
---
+++ blogs/src/main/kotlin/team/odds/blogs/models/Post.kt
@@ -0,0 +1,5 @@
+package team.odds.blogs.models
+
+class Post (val id: Int, val userId: Int, val title: String, val body: String) {
+
+}
